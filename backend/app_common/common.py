f68e3ccbd6531881538e36d79052a640e1a29d52
Thirumal-J <Thirumal.Janakiraman@gmail.com>
2020-12-06 19:21:03 +0100

    Added AppCommon folder for BACKEND Development

---
+++ backend/app_common/common.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+import constants as const
+
+response = {"status_code": "", "status_msg": "", "status_type":"", "data":""}
+
+def createJSONResponse(outputJson, status_code= const.SUCCESS_CODE,status_msg=const.DEFAULT_SUCCESS_MSG, status_type=const.SUCCESS):
+    response["status_code"] = status_code
+    response["status_msg"] =  status_msg
+    response["status_type"] = status_type
+    response["data"] = outputJson
+    return jsonify(response)
+
